1514ff6a385cda1a3fa73ad3121fb6d0bc52725d
Daniel Ramirez <notused@notused.net>
2021-06-17 21:11:30 -0700

    Update validation of auth

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
 function validateLogin(req) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(1024).required(),
+    password: Joi.string().max(1024).required(),
   });
   return schema.validate(req);
 }
